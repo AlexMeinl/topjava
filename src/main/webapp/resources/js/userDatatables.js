2e836f3e08e7ca7892b771c322a145aa735e20c2
AlexMeinl <kds30@inbox.ru>
2017-06-14 22:00:49 +0300

    Prepare HEROKU

--- src/main/webapp/resources/js/userDatatables.js
+++ src/main/webapp/resources/js/userDatatables.js
@@ -1,34 +1,72 @@
 var ajaxUrl = 'ajax/admin/users/';
 var datatableApi;
 
+function updateTable() {
+    $.get(ajaxUrl, updateTableByData);
+}
+
+function enable(chkbox, id) {
+    var enabled = chkbox.is(":checked");
+    $.ajax({
+        url: ajaxUrl + id,
+        type: 'POST',
+        data: 'enabled=' + enabled,
+        success: function () {
+            chkbox.closest('tr').toggleClass('disabled');
+            successNoty(enabled ? 'common.enabled' : 'common.disabled');
+        },
+        error: function () {
+            $(chkbox).prop("checked", !enabled);
+        }
+    });
+}
+
 // $(document).ready(function () {
 $(function () {
-    datatableApi = $('#datatable').DataTable({
-        "paging": false,
-        "info": true,
+    datatableApi = $('#datatable').DataTable(extendsOpts({
         "columns": [
             {
                 "data": "name"
             },
             {
-                "data": "email"
+                "data": "email",
+                "render": function (data, type, row) {
+                    if (type === 'display') {
+                        return '<a href="mailto:' + data + '">' + data + '</a>';
+                    }
+                    return data;
+                }
             },
             {
                 "data": "roles"
             },
             {
-                "data": "enabled"
+                "data": "enabled",
+                "render": function (data, type, row) {
+                    if (type === 'display') {
+                        return '<input type="checkbox" ' + (data ? 'checked' : '') + ' onclick="enable($(this),' + row.id + ');"/>';
+                    }
+                    return data;
+                }
             },
             {
-                "data": "registered"
+                "data": "registered",
+                "render": function (date, type, row) {
+                    if (type === 'display') {
+                        return '<span>' + date.substring(0, 10) + '</span>';
+                    }
+                    return date;
+                }
             },
             {
-                "defaultContent": "Edit",
-                "orderable": false
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderEditBtn
             },
             {
-                "defaultContent": "Delete",
-                "orderable": false
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderDeleteBtn
             }
         ],
         "order": [
@@ -36,7 +74,11 @@ $(function () {
                 0,
                 "asc"
             ]
-        ]
-    });
-    makeEditable();
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            if (!data.enabled) {
+                $(row).addClass("disabled");
+            }
+        }
+    }));
 });
